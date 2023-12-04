62ea11a5c6ca5b7643c0e6305c6d96b8c24e0433
liftcodesleep <liftcodesleep@gmail.com>
2023-12-04 10:58:55 -0800

    apply prefabs

--- Assets/Scripts/CardZone/TestDeck1.cs
+++ Assets/Scripts/CardZone/TestDeck1.cs
@@ -6,26 +6,39 @@ public class TestDeck1 : CardZone {
   public TestDeck1(Player Owner) {
     this.Owner = Owner;
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     this.Cards.Add(new AncestralRecall(this.Owner));
-    this.Cards.Add(new AncestralRecall(this.Owner));
-    this.Cards.Add(new AncestralRecall(this.Owner));
-    this.Cards.Add(new AncestralRecall(this.Owner));
-    this.Cards.Add(new AncestralRecall(this.Owner));
+    this.Cards.Add(new Migo(this.Owner));
     Shuffle(this.Cards);
   }
 }
